e136805236b93f95e02430074924bb72427a784c
李毅 <liyi1472@163.com>
2021-05-01 23:54:56 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,24 @@
+# 百度纯净版
+
+配合 Edge 插件 ["百度药丸 Baidu Capsule"](https://microsoftedge.microsoft.com/addons/detail/百度药丸-baidu-capsule/hmmpaaklbinfllncmnnbdhedmhafnjkj?hl=zh-CN) 使用效果更佳。
+
+
+
+### 致敬
+
+- 简单搜索，一个简单的前端界面。
+
+  https://github.com/5iux/sou
+
+  - **PHP版本**
+
+    链接：https://5iux.cn
+
+  - **静态版本**
+
+    链接1：https://cn5iux.gitee.io
+
+    链接2：https://5iux.github.io
+
+    链接3：https://5iux.netlify.app
+
---
+++ js/master.js
@@ -0,0 +1,25 @@
+// 初始化
+$(function () {
+
+  // 关键字文本框
+  $('#keyword').on('keydown', function () {
+    if (event.keyCode == 13) {
+      // 回车搜索
+      $("#search").click();
+    }
+  });
+
+  // 搜索按钮
+  $('#search').on('click', function () {
+    if ($('#keyword').val()) {
+      // 搜索关键字
+      var url = 'https://www.baidu.com/s?ie=utf-8&wd=';
+      url += $('#keyword').val();
+      location.href = url;
+    } else {
+      // 空关键字刷新
+      location.reload();
+    }
+  });
+
+});
